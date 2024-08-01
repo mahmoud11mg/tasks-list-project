4b9b0cd0dc7df962ba71c033b70dea00b9477acc
mahmoud11mg <mahmoud11magd@gmail.com>
2024-08-02 00:49:27 +0200

    Supplement Leasson 54 - Server Action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-2xl'>HomePage</div>
+    <section className='text-2xl'>
+      <h1 className='text-4xl font-semibold'> HomePage </h1>
+      <div className='flex items-center justify-end mb-20'> <Link href={"/task/add"} className='bg-cyan-300 hover:bg-cyan-500 text-black font-semibold text-xl rounded-md p-2 transition-colors'>Add Task</Link></div>
+      
+    
+    </section>
   )
 }
 
